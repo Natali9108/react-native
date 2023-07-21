de7f6b6ef2fd0cd674a39f90a54feeee4f188f44
Natali <114168847+Natali9108@users.noreply.github.com>
2023-07-21 15:08:54 +0300

    Update App.js

--- App.js
+++ App.js
@@ -19,8 +19,8 @@ export default function App() {
       accessibilityIgnoresInvertColors={true}
       style={styles.safeAreaContainer}
     >
-      {/* <RegistrationScreen /> */}
-      <LoginScreen />
+      <RegistrationScreen />
+      {/* <LoginScreen /> */}
 
       <StatusBar />
     </SafeAreaView>
